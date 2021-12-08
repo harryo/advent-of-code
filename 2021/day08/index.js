fe917a0bd3dcbfec40d840ab0b6c0b0babf1f060
Harry Oosterveen <mail@harryonline.net>
2021-12-08 15:39:53 +0100

    Day 8

---
+++ 2021/day08/index.js
@@ -0,0 +1,93 @@
+const readlines = require('../../helpers/readLines');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+function splitInput(line) {
+  const [patterns, output] = line.split('|');
+  return {
+    patterns: patterns.split(/\s+/).filter(Boolean),
+    output: output.split(/\s+/).filter(Boolean),
+  };
+}
+
+const source = readlines().filter(Boolean).map(splitInput);
+const digitSegments = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg'];
+const chars = 'abcdefg'.split('');
+
+function matchingDigits(pat) {
+  return digitSegments.filter((o) => o.length === pat.length);
+}
+
+function solve1() {
+  return source.map((s) => s.output).flat().filter((o) => matchingDigits(o).length === 1).length;
+}
+
+function getInitialOptions() {
+  return chars.reduce((acc, ch) => ({ ...acc, [ch]: chars }), {});
+}
+
+/**
+ * Transform a pattern into a digit
+ * @param {str} pattern
+ * @param {object} options
+ * @returns
+ */
+function transform(pattern, options) {
+  const transformed = pattern.split('').map((c) => options[c][0]).sort().join('');
+  const result = digitSegments.findIndex((str) => str === transformed);
+  return result;
+}
+
+/**
+ * Just try each option and see whether it results in valid digits
+ * @param {*} patterns
+ * @param {*} options
+ * @returns
+ */
+function tryOut(patterns, options) {
+  // Find a char for which there are still multiple options
+  const choice = chars.find((ch) => options[ch].length > 1);
+  if (!choice) {
+    return patterns.every((pat) => transform(pat, options) !== -1) ? options : null;
+  }
+  let solution = null;
+  options[choice].some((tryChar) => {
+    const nextAttempt = {};
+    chars.forEach((ch) => {
+      nextAttempt[ch] = ch === choice ? [tryChar] : options[ch].filter((c) => c !== tryChar);
+    });
+    solution = tryOut(patterns, nextAttempt);
+    return solution;
+  });
+  return solution;
+}
+
+function deduce({ patterns, output }) {
+  const options = getInitialOptions();
+  patterns.forEach((pat) => {
+    const digits = matchingDigits(pat);
+    if (digits.length > 1) {
+      return;
+    }
+    chars.forEach((ch) => {
+      if (pat.includes(ch)) {
+        // ch should refer to one of the chars in digit
+        options[ch] = options[ch].filter((c) => digits[0].includes(c));
+      } else {
+        // ch cannot refer to one of the chars in digit
+        options[ch] = options[ch].filter((c) => !digits[0].includes(c));
+      }
+    });
+  });
+  const solution = tryOut(patterns, options);
+
+  const result = output.map((str) => ({ str, value: transform(str, solution) }));
+  return parseInt(result.map((o) => o.value).join(''), 10);
+}
+
+function solve2() {
+  return source.reduce((sum, o) => sum + deduce(o), 0);
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
