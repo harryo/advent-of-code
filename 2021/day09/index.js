9980627944fb73a6c7a5cfa9d80da045759e86e2
Harry Oosterveen <mail@harryonline.net>
2021-12-09 13:45:40 +0100

    Day 9

---
+++ 2021/day09/index.js
@@ -0,0 +1,70 @@
+const readLines = require('../../helpers/readLines');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+let index = 0;
+
+// Create map with for each location its position, height and a unique index
+const heightMap = readLines()
+  .map((line, r) => line
+    .split('')
+    .filter(Boolean)
+    .map((v, c) => ({
+      r, c, h: Number(v), i: index++,
+    })));
+
+/**
+ * Find adjacent locations
+ * @param {*} loc
+ * @returns
+ */
+function adjacent(loc) {
+  const { r, c } = loc;
+  return [[-1, 0], [1, 0], [0, -1], [0, 1]] // possible directions
+    .map((d) => ({ r: r + d[0], c: c + d[1] })) // possible positions
+    .map((p) => heightMap[p.r] && heightMap[p.r][p.c]) // to map locations
+    .filter(Boolean); // only inside map
+}
+
+function findLowPoints() {
+  return heightMap
+    .flat()
+    .filter((loc) => adjacent(loc)
+      .every((o) => o.h > loc.h));
+}
+
+let lowPoints;
+
+function solve1() {
+  lowPoints = findLowPoints();
+  return lowPoints.reduce((sum, o) => sum + o.h + 1, 0);
+}
+
+function getBasin(lowPoint) {
+  const checkedLocations = new Set();
+
+  function extend(loc) {
+    if (checkedLocations.has(loc.i)) {
+      return [];
+    }
+    checkedLocations.add(loc.i);
+    const newLocations = adjacent(loc)
+      .filter((o) => o.h !== 9 && o.h > loc.h)
+      .map((o) => extend(o))
+      .flat();
+    return [loc, ...newLocations];
+  }
+
+  return extend(lowPoint);
+}
+
+function solve2() {
+  return lowPoints
+    .map(getBasin)
+    .sort((a, b) => b.length - a.length)
+    .slice(0, 3)
+    .reduce((p, b) => p * b.length, 1);
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
