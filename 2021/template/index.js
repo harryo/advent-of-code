c6cb142be97d980d9e3f115fef217f4bebd9dd27
Harry Oosterveen <mail@harryonline.net>
2021-12-09 14:36:59 +0100

    Add template

---
+++ 2021/template/index.js
@@ -0,0 +1,15 @@
+// const readFile = require('../../helpers/readFile');
+// const readLines = require('../../helpers/readLines');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+function solve1() {
+  return 'Pending';
+}
+
+function solve2() {
+  return 'Pending';
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
