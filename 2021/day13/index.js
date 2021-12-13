6aa4afee3f88303889cf1d0712bcf6782642df82
Harry Oosterveen <mail@harryonline.net>
2021-12-13 12:41:49 +0100

    Day 13

---
+++ 2021/day13/index.js
@@ -0,0 +1,54 @@
+const readFile = require('../../helpers/readFile');
+// const readLines = require('../../helpers/readLines');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+const blocks = readFile().split(/\n{2,}/);
+
+// Keep points as string to easily find duplicates
+const points = blocks[0].split(/\n/);
+
+// Get key (x or y) and value for folds
+const folds = blocks[1].split(/\n/)
+  .map((line) => line.match(/([xy])=(\d+)/))
+  .map((match) => ({ key: match[1], value: Number(match[2]) }));
+
+// Make a fold and return unique points
+function makeFold(pts, fold) {
+  const { key, value } = fold;
+  const newPoints = pts.map((pt) => {
+    const [x, y] = pt.split(',').map(Number);
+    const newPoint = { x, y };
+    newPoint[key] = newPoint[key] < value ? newPoint[key] : 2 * value - newPoint[key];
+    return `${newPoint.x},${newPoint.y}`;
+  });
+  // Eliminate double points
+  return Array.from(new Set(newPoints));
+}
+
+// Create a mtraix with the points marked
+function asString(strPoints) {
+  const pts = strPoints.map((p) => p.split(',').map(Number)).map(([x, y]) => ({ x, y }));
+  const size = {
+    x: Math.max(...pts.map((p) => p.x)),
+    y: Math.max(...pts.map((p) => p.y)),
+  };
+  const matrix = Array(size.y + 1).fill().map(() => Array(size.x + 1).fill('.'));
+  pts.forEach((pt) => {
+    matrix[pt.y][pt.x] = '#';
+  });
+  const str = matrix.map((line) => line.join('')).join('\n');
+  return `\n${str}`;
+}
+
+function solve1() {
+  return makeFold(points, folds[0]).length;
+}
+
+function solve2() {
+  const final = folds.reduce((pts, fold) => makeFold(pts, fold), points);
+  return asString(final);
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
