a3078c3be2e0bc7ba790fbc2a072ee658e93c2ba
Harry Oosterveen <mail@harryonline.net>
2021-12-19 19:28:39 +0100

    Fix sortBy to handle strings

--- helpers/sortBy.js
+++ helpers/sortBy.js
@@ -4,15 +4,22 @@
  * @returns
  */
 
+function compare(a, b) {
+  if (a > b) {
+    return 1;
+  }
+  return a < b ? -1 : 0;
+}
+
 function sortBy(arg) {
   if (typeof arg === 'function') {
-    return this.sort((a, b) => arg(a) - arg(b));
+    return this.sort((a, b) => compare(arg(a), arg(b)));
   }
   if (typeof arg === 'string' && arg[0] === '-') {
     const prop = arg.slice(1);
-    return this.sort((a, b) => b[prop] - a[prop]);
+    return this.sort((a, b) => compare(b[prop], a[prop]));
   }
-  return this.sort((a, b) => a[arg] - b[arg]);
+  return this.sort((a, b) => compare(a[arg], b[arg]));
 }
 
 if (!Array.prototype.sortBy) {
