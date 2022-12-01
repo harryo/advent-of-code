503ec047cb005c83fe342bfdfd29dcf6fe704269
harry.oosterveen <mail@harryonline.net>
2022-12-01 10:39:46 +0100

    Update for 2022

---
+++ 2022/day01/index.js
@@ -0,0 +1,19 @@
+// const readFile = require('../../helpers/readFile');
+// const readLines = require('../../helpers/readLines');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+console.time('Preparation');
+
+console.timeEnd('Preparation');
+
+function solve1() {
+  return 'Pending';
+}
+
+function solve2() {
+  return 'Pending';
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
