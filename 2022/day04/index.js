eb6eca64ec55aff8152f8a20f976de96479b5cf5
harry.oosterveen <mail@harryonline.net>
2022-12-05 10:58:53 +0100

    Day 04

---
+++ 2022/day04/index.js
@@ -0,0 +1,29 @@
+// const readFile = require('../../helpers/readFile');
+const readLines = require('../../helpers/readLines');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+console.time('Preparation');
+
+const data = readLines()
+  .map((line) => line.split(/\D+/).map(Number))
+  .map(([min1, max1, min2, max2]) => [{ min: min1, max: max1 }, { min: min2, max: max2 }]);
+
+console.timeEnd('Preparation');
+
+function solve1() {
+  function fullOverlap(r1, r2) {
+    return r1.min <= r2.min && r1.max >= r2.max;
+  }
+  return data.filter(([r1, r2]) => fullOverlap(r1, r2) || fullOverlap(r2, r1)).length;
+}
+
+function solve2() {
+  function partOverlap(r1, r2) {
+    return r1.min <= r2.max && r1.max >= r2.min;
+  }
+  return data.filter(([r1, r2]) => partOverlap(r1, r2)).length;
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
