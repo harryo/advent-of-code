ca8d6f00bb9e7e84e1a7e291248381c1cab635ff
harry.oosterveen <mail@harryonline.net>
2022-12-05 10:59:04 +0100

    Day 05

---
+++ 2022/day05/index.js
@@ -0,0 +1,46 @@
+const readBlocks = require('../../helpers/readBlocks');
+const showTimedSolution = require('../../helpers/showTimedSolution');
+
+function getState(block) {
+  const [base, ...columns] = block.split(/\n/).reverse();
+  const result = {};
+  base.split('').forEach((key, index) => {
+    if (key === ' ') {
+      return;
+    }
+    result[key] = columns.map((column) => column[index]).filter((char) => char && char !== ' ');
+  });
+  return result;
+}
+
+console.time('Preparation');
+
+const data = readBlocks();
+
+const moves = data[1].split(/\n/).map((line) => line.match(/\d+/g).map(Number));
+
+console.timeEnd('Preparation');
+
+function solve1() {
+  const state = getState(data[0]);
+  moves.forEach(([count, from, to]) => {
+    for (let i = 0; i < count; i++) {
+      const crate = state[from].pop();
+      state[to].push(crate);
+    }
+  });
+  return Object.values(state).map((l) => l.pop()).join('');
+}
+
+function solve2() {
+  const state = getState(data[0]);
+  moves.forEach(([count, from, to]) => {
+    const pile = state[from].splice(-count);
+    state[to].push(...pile);
+  });
+  return Object.values(state).map((l) => l.pop()).join('');
+}
+
+showTimedSolution(1, () => solve1());
+
+showTimedSolution(2, () => solve2());
