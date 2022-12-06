41104ba8f6f58c1664af98c0e7916914cbd8a2c4
harry.oosterveen <mail@harryonline.net>
2022-12-06 08:42:17 +0100

    Day 06

--- 2022/day06/index.js
+++ 2022/day06/index.js
@@ -1,17 +1,40 @@
-// const readFile = require('../../helpers/readFile');
+const readFile = require('../../helpers/readFile');
 // const readLines = require('../../helpers/readLines');
 const showTimedSolution = require('../../helpers/showTimedSolution');
 
+/**
+ * Return index of first character that is duplicated in arr
+ * @param {array} arr
+ * @returns int
+ */
+function findDuplicate(arr) {
+  return arr.slice(0, -1).findIndex((ch, i) => arr.slice(i + 1).includes(ch));
+}
+
+function findMarker(arr, size) {
+  let idx = 0;
+  while (idx < arr.length) {
+    const duplicateIndex = findDuplicate(arr.slice(idx, idx + size));
+    if (duplicateIndex === -1) {
+      break; // Found marker
+    }
+    idx += duplicateIndex + 1;
+  }
+  return idx + size;
+}
+
 console.time('Preparation');
+
+const data = readFile().split('');
 
 console.timeEnd('Preparation');
 
 function solve1() {
-  return 'Pending';
+  return findMarker(data, 4);
 }
 
 function solve2() {
-  return 'Pending';
+  return findMarker(data, 14);
 }
 
 showTimedSolution(1, () => solve1());
