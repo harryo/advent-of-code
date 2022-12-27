491cae0ffd36d51be467f73031b86c930ace1277
harry.oosterveen <mail@harryonline.net>
2022-12-27 18:10:58 +0100

    Day 25

--- 2022/day25/index.js
+++ 2022/day25/index.js
@@ -3,10 +3,29 @@ const timedLog = require('../../helpers/timedLog');
 
 const data = readLines();
 
+const base = '=-012';
+
+const val = (c) => base.indexOf(c) - 2;
+
+function toDecimal(str) {
+  return str.split('').reduce((acc, c) => acc * 5 + val(c), 0);
+}
+
+function toSnafu(num) {
+  const digits = [0, ...num.toString(5).split('').map(Number)];
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] >= 3) {
+      digits[i] -= 5;
+      digits[i - 1]++;
+    }
+  }
+  return digits.map((d) => base[d + 2]).join('').replace(/^0/, '');
+}
+
 timedLog('Preparation');
 
 function solve1() {
-  return 'Pending';
+  return toSnafu(data.map(toDecimal).reduce((acc, n) => acc + n, 0));
 }
 
 function solve2() {
