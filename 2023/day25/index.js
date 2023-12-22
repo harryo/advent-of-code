5923275de3d6e2120f26d4135a2dfa1363b90b58
Harry Oosterveen <mail@harryonline.net>
2023-12-22 23:33:51 +0100

    Added empty days

---
+++ 2023/day25/index.js
@@ -0,0 +1,17 @@
+import { readLines } from '../../helpers/readInput/index.js';
+import timedLog from '../../helpers/timedLog.js';
+
+const data = readLines();
+
+timedLog('Preparation');
+
+function solve1() {
+  return 'Pending';
+}
+
+function solve2() {
+  return 'Pending';
+}
+
+timedLog('Part 1:', solve1());
+timedLog('Part 2:', solve2());
